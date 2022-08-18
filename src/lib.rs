8aa38e82a95abc749a1bd2acb6b38cdb1e230630
Kotaro7750 <7750koutarou@gmail.com>
2022-08-18 21:46:02 +0900

    Add: ライブラリとして外部に公開する構造体のひな型を定義した

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use query::QueryRequest;
+
 mod chunk;
 mod chunk_key_stroke_dictionary;
 mod key_stroke;
@@ -8,3 +10,32 @@ mod vocabulary;
 
 #[cfg(test)]
 mod test_utility;
+
+/// The main engine of typing game.
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct TypingEngine {}
+
+impl TypingEngine {
+    /// Construct an empty engine.
+    ///
+    /// This method only do construct typing engine, so you must call [`init`](Self::init()) method to construct
+    /// query and [`start`](Self::start()) method to start typing.
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    /// Construct query using [`QueryRequest`].
+    pub fn init(&mut self, query_request: QueryRequest) {
+        unimplemented!();
+    }
+
+    /// Append query using [`QueryRequest`].
+    pub fn append_query(&mut self, query_request: QueryRequest) {
+        unimplemented!();
+    }
+
+    /// Start typing.
+    pub fn start(&mut self) {
+        unimplemented!();
+    }
+}
