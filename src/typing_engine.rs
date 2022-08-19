5ca93ca2bfb4002ab27b23c2462cff3950bb694a
Kotaro7750 <7750koutarou@gmail.com>
2022-08-19 13:12:08 +0900

    Add: TypingEngineのstartメソッドを実装した

--- src/typing_engine.rs
+++ src/typing_engine.rs
@@ -1,5 +1,7 @@
+use std::collections::VecDeque;
 use std::error::Error;
 use std::fmt::Display;
+use std::time::Instant;
 
 use crate::chunk::Chunk;
 use crate::chunk::TypedChunk;
@@ -58,6 +60,7 @@ enum TypingEngineState {
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct TypingEngine {
     state: TypingEngineState,
+    start_time: Option<Instant>,
     processed_chunk_info: Option<ProcessedChunkInfo>,
     vocabulary_infos: Option<Vec<VocabularyInfo>>,
 }
@@ -70,6 +73,7 @@ impl TypingEngine {
     pub fn new() -> Self {
         Self {
             state: TypingEngineState::Uninitialized,
+            start_time: None,
             processed_chunk_info: None,
             vocabulary_infos: None,
         }
@@ -117,8 +121,26 @@ impl TypingEngine {
     }
 
     /// Start typing.
+    ///
+    /// If this method is called before initializing via calling [`init`](Self::init()) method, this
+    /// method returns error.
     pub fn start(&mut self) -> Result<(), TypingEngineError> {
-        unimplemented!();
+        if self.is_initialized() {
+            assert!(self.processed_chunk_info.is_some());
+            assert!(self.vocabulary_infos.is_some());
+
+            self.processed_chunk_info
+                .as_mut()
+                .unwrap()
+                .move_next_chunk();
+
+            self.start_time.replace(Instant::now());
+            Ok(())
+        } else {
+            Err(TypingEngineError::new(
+                TypingEngineErrorKind::MustBeInitialized,
+            ))
+        }
     }
 
     fn is_initialized(&self) -> bool {
@@ -131,7 +153,7 @@ impl TypingEngine {
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub(crate) struct ProcessedChunkInfo {
-    unprocessed_chunks: Vec<Chunk>,
+    unprocessed_chunks: VecDeque<Chunk>,
     inflight_chunk: Option<TypedChunk>,
     confirmed_chunks: Vec<TypedChunk>,
 }
@@ -139,13 +161,31 @@ pub(crate) struct ProcessedChunkInfo {
 impl ProcessedChunkInfo {
     pub(crate) fn new(chunks: Vec<Chunk>) -> Self {
         Self {
-            unprocessed_chunks: chunks,
+            unprocessed_chunks: chunks.into(),
             inflight_chunk: None,
             confirmed_chunks: vec![],
         }
     }
 
-    pub(crate) fn append_chunks(&mut self, mut chunks: Vec<Chunk>) {
-        self.unprocessed_chunks.append(&mut chunks);
+    pub(crate) fn append_chunks(&mut self, chunks: Vec<Chunk>) {
+        self.unprocessed_chunks.append(&mut (chunks.into()));
+    }
+
+    // 現在打っているチャンクを確定させ未処理のチャンク列の先頭のチャンクの処理を開始する
+    pub(crate) fn move_next_chunk(&mut self) {
+        // まずは現在打っているチャンクを確定済みチャンク列に追加する
+        if self.inflight_chunk.is_some() {
+            // XXX 現在打っているチャンクが終了しているかを確かめる必要がある
+
+            let current_inflight_chunk = self.inflight_chunk.take().unwrap();
+            self.confirmed_chunks.push(current_inflight_chunk);
+        }
+
+        assert!(self.inflight_chunk.is_none());
+
+        // 未処理チャンク列の先頭チャンクを処理中のチャンクにする
+        if let Some(next_inflight_chunk) = self.unprocessed_chunks.pop_front() {
+            self.inflight_chunk.replace(next_inflight_chunk.into());
+        }
     }
 }
